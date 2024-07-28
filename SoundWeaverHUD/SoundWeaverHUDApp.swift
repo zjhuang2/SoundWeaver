71c308ab72528892ae5e09757096af9db9350a4a
Jeremy Huang <jeremy.zq.huang@gmail.com>
2024-07-28 15:20:10 -0400

    Initial Commit

---
+++ SoundWeaverHUD/SoundWeaverHUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundWeaverHUDApp.swift
+//  SoundWeaverHUD
+//
+//  Created by Jeremy Huang on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundWeaverHUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
